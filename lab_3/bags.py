5ad7d71de5c497e8448acad93b6aa5756fb1a16c
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-03-06 05:42:05 +0200

    lab_3_task_1 v.1.1

--- lab_3/bags.py
+++ lab_3/bags.py
@@ -5,22 +5,17 @@ from sklearn.feature_extraction.text import CountVectorizer
 def bag_of_words(prepared_corpus):
     cv = CountVectorizer(min_df=0., max_df=1.)
     matrix = cv.fit_transform(prepared_corpus)
-    return matrix.toarray()
+    return pd.DataFrame(matrix.toarray(), columns=cv.get_feature_names_out())
 
 
-def bag_of_ngrams(prepared_corpus):
+def bag_of_bigrams(prepared_corpus):
     cv = CountVectorizer(ngram_range=(2, 2))
     matrix = cv.fit_transform(prepared_corpus)
-    return matrix
+    return pd.DataFrame(matrix.toarray(), columns=cv.get_feature_names_out())
 
 
-def roman_architecture_vector(prepared_corpus):
+def phrase_vector(prepared_corpus, phrase):
     cv = CountVectorizer(ngram_range=(2, 2))
-    cv.fit_transform(prepared_corpus)
-    query = ["Roman architecture"]
-    query_vec = cv.transform(query)
-    return query_vec.toarray(), cv.vocabulary_
-
-
-def to_data_frame(matrix, vocabulary):
-    return pd.DataFrame(matrix, columns=vocabulary)
+    df = pd.DataFrame(cv.fit_transform(prepared_corpus).toarray(), columns=cv.get_feature_names_out())
+    vector = df[phrase.lower()].tolist()
+    return vector
--- lab_3/main.py
+++ lab_3/main.py
@@ -1,4 +1,6 @@
 import text_pre_processing as tpp
+import bags
+import pandas as pd
 
 with open('files/doc5.txt', 'r') as file:
     lines = []
@@ -6,8 +8,12 @@ with open('files/doc5.txt', 'r') as file:
         lines.append(line.strip())
 corpus = list(filter(None, lines))
 prepared_corpus = tpp.end_preprocessing(corpus)
+
 print(prepared_corpus)
 
+df = bags.bag_of_bigrams(prepared_corpus)
+df.to_csv('files/ngrams.csv')
 
-
+print("Vector for 'Roman architecture':")
+print(bags.phrase_vector(prepared_corpus, "Roman architecture"))
 
