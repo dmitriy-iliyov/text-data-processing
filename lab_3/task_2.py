9c7a8044b9a39da093e56df0aebe03cb41c0fa36
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-03-06 20:12:44 +0200

    upd lab_3/task_2.py

---
+++ lab_3/task_2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def to_tf_idf(prepared_corpus):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    tv = TfidfVectorizer(min_df=0., max_df=1., norm='l2', use_idf=True, smooth_idf=True)
+    tv_matrix = tv.fit_transform(prepared_corpus)
+    return pd.DataFrame(np.round(tv_matrix.toarray(), 2), columns=tv.get_feature_names_out())
+
+
+def clusterizate(prepared_corpus):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.cluster import AgglomerativeClustering
+    tv = TfidfVectorizer(min_df=0., max_df=1., norm='l2', use_idf=True, smooth_idf=True)
+    tfidf_matrix = tv.fit_transform(prepared_corpus)
+    clustering = AgglomerativeClustering(n_clusters=2)
+    clusters = clustering.fit_predict(tfidf_matrix.toarray())
+    return clusters
