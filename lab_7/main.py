4166171181f3752ec687a005b9f0c47ee748c49f
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-05-15 04:36:25 +0300

    lab 7

---
+++ lab_7/main.py
@@ -0,0 +1,14 @@
+from task_1 import Task1
+
+
+# task 1
+t1 = Task1()
+t1.learn_model()
+# task.test_model("The movie Titanic won 11 Academy Awards.")
+t1.test_model("The movie Titanic won 11 Academy Awards, including Best Picture and Best Director. The album 21 by Adele "
+              "received six Grammy Awards. The TV series Game of Thrones won numerous Emmy Awards during its run. "
+              "The book The Great Gatsby was awarded the Pulitzer Prize for Fiction. The film La La Land received six "
+              "Academy Award nominations. The artist Taylor Swift has won several American Music Awards.")
+
+# task 2
+
