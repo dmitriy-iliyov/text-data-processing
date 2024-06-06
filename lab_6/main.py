c5e480bb38b675006fa2d8b1fe7f341d82f1b539
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-06-06 11:29:24 +0300

    v1.1

--- lab_6/main.py
+++ lab_6/main.py
@@ -9,8 +9,8 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
 # task_1
-# t1 = Task1()
-# t1.do_task()
+t1 = Task1()
+t1.do_task()
 # task_2
 t2 = Task2()
 t2.do_task()
