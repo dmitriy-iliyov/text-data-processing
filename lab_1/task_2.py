c45cee9212dba6601c89b6006dd50f1ee66a74eb
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-06-06 11:23:19 +0300

    v3

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -2,8 +2,7 @@ import re
 
 
 def do(data):
-    print(data)
-    data = re.sub(r'([^\w\s])([^\w\s]{1,100})', lambda x: x.group(1), data)  #
+    data = re.sub(r'([^\w\s])([^\w\s]{1,100})', lambda x: x.group(1), data)
     data = re.sub(r'\s+', ' ', data)
     data = re.sub(r'([A-Z])([a-zA-Z]{1,100})', lambda x: x.group(1) + x.group(2).lower(), data)
     print(data)
